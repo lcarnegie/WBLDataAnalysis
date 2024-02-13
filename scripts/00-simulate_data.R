fda1c7458fbef4124c82207554469f1f19cee78d
SEHB2012 <sehar.bajwa@mail.utoronto.ca>
2024-02-13 22:17:53 +0000

    Completed simulated dataframe and tests

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,56 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates a table with Canada's WBL scores across 8 indicators and 10 years (2013-2023)
+# Author: Luca Carnegie and Sehar Bajwa
+# Date: 13 February 2024 
+# Contact: luca.carnegie@mail.utoronto.ca
+#           sehar.bajwa@mail.utoronto.ca          
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: 
+# Any other information needed 
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+# Creating a simulated database for Canada scores in WBL
+set.seed(123)  # Setting seed for reproducibility
 
+# Generating report years from 2010 to 2020
+report_years <- 2010:2020
+
+# Generating random scores for the specified columns 
+#Can only be 0, 25, 50, 75, 100 due to nature of survey
+scores <- sample(c(0, 25, 50, 75, 100), length(report_years), replace = TRUE)
+
+# Creating the data frame
+simulated_data <- data.frame(
+  Economy = rep("Canada", length(report_years)),
+  Region = rep("High income: OECD", length(report_years)),
+  Report_Year = report_years,
+  WBL_Index = scores,
+  Mobility = scores,
+  Workplace = scores,
+  Pay = scores,
+  Marriage = scores,
+  Parenthood = scores,
+  Entrepreneurship = scores,
+  Assets = scores,
+  Pension = scores
+)
+
+# Testing simulated data
+#simulated_data$Report_Year |> min() == 2010
+#simulated_data$Report_Year |> max() == 2020
+#simulated_data$WBL_Index |> min() == 0
+
+#simulated_data$Mobility %in% c(0, 25, 50, 75, 100)
+#simulated_data$Workplace %in% c(0, 25, 50, 75, 100)
+#simulated_data$Pay %in% c(0, 25, 50, 75, 100)
+#simulated_data$Marriage %in% c(0, 25, 50, 75, 100)
+#simulated_data$Parenthood %in% c(0, 25, 50, 75, 100)
+#simulated_data$Entrepreneurship %in% c(0, 25, 50, 75, 100)
+#simulated_data$Assets %in% c(0, 25, 50, 75, 100)
+#simulated_data$Pension %in% c(0, 25, 50, 75, 100)
 
 
