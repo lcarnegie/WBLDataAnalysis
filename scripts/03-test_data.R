56e25143cbb94584b93771e9b1318fb1c8256e9c
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2024-02-18 11:29:32 -0500

    tested data

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,11 +1,8 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Runs various unit tests on wb_data_clean.csv
+# Author: Luca Carnegie
+# Date: February 18 2024
+# Contact: luca.carnegie@mail.utoronto.ca
 
 
 #### Workspace setup ####
@@ -16,22 +13,23 @@ wb_data_clean <- read_csv("outputs/data/wb_data_clean.csv")
 
 #### Run similar tests to simulated data  ####
 
+# For the specified columns, we test...
+
 # Does the year fall between 1971-2020?
 wb_data_clean$Report_Year |> min() == 1971
 wb_data_clean$Report_Year |> max() == 2020
 
-#Does the WBL index stay at or higher than 0?
-wb_data_clean$WBL_Index |> min() == 0
+# Does the WBL index stay at or higher than 0?
+wb_data_clean$WBL_Index |> min() >= 0
 
-# Do all the index values fall between 0 and 100 (as spec'd in the paper)?
+# Do all the WBL index values fall between 0 and 100 (as spec'd in the paper)?
 
-#TODO: fix the c() so that it is a range(0-100)
+all (wb_data_clean$GR1_mobility >= 0 & wb_data_clean$GR1_mobility <= 100)
+all (wb_data_clean$GR2_workplace >= 0 & wb_data_clean$GR2_workplace <= 100)
+all (wb_data_clean$GR3_pay >= 0 & wb_data_clean$GR3_pay <= 100)
+all (wb_data_clean$GR4_marriage >= 0 & wb_data_clean$GR4_marriage <= 100)
+all (wb_data_clean$GR5_parenthood >= 0 & wb_data_clean$GR5_parenthood <= 100)
+all (wb_data_clean$GR6_entrepreneurship >= 0 & wb_data_clean$GR6_entrepreneurship <= 100)
+all (wb_data_clean$GR7_assets >= 0 & wb_data_clean$GR7_assets <= 100)
+all (wb_data_clean$GR8_pension >= 0 & wb_data_clean$GR8_pension <= 100)
 
-wb_data_clean$GR1_mobility %in% c(0, 25, 50, 75, 100) 
-wb_data_clean$GR2_workplace %in% c(0, 25, 50, 75, 100)
-wb_data_clean$GR3_pay %in% c(0, 25, 50, 75, 100)
-wb_data_clean$GR4_marriage %in% c(0, 25, 50, 75, 100)
-wb_data_clean$GR5_parenthood %in% c(0, 25, 50, 75, 100)
-wb_data_clean$GR6_entrepreneurship %in% c(0, 25, 50, 75, 100)
-wb_data_clean$GR7_assets %in% c(0, 25, 50, 75, 100)
-wb_data_clean$GR8_pension %in% c(0, 25, 50, 75, 100)
